cf76234c8157a74b62c3a9a6c31bbf8472a9e603
Marcio Martins <marciopmm@gmail.com>
2015-05-30 19:42:59 -0300

    Carga inicial

---
+++ DindinCounter/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace DindinCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.btnSacar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl20 = new System.Windows.Forms.Label();
+            this.lbl50 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(96, 25);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 20);
+            this.txtValor.TabIndex = 0;
+            // 
+            // btnSacar
+            // 
+            this.btnSacar.Location = new System.Drawing.Point(202, 23);
+            this.btnSacar.Name = "btnSacar";
+            this.btnSacar.Size = new System.Drawing.Size(75, 23);
+            this.btnSacar.TabIndex = 1;
+            this.btnSacar.Text = "Sacar";
+            this.btnSacar.UseVisualStyleBackColor = true;
+            this.btnSacar.Click += new System.EventHandler(this.btnSacar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Valor do saque";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Notas de 20:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Notas de 50:";
+            // 
+            // lbl20
+            // 
+            this.lbl20.AutoSize = true;
+            this.lbl20.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl20.Location = new System.Drawing.Point(93, 81);
+            this.lbl20.Name = "lbl20";
+            this.lbl20.Size = new System.Drawing.Size(19, 20);
+            this.lbl20.TabIndex = 5;
+            this.lbl20.Text = "0";
+            // 
+            // lbl50
+            // 
+            this.lbl50.AutoSize = true;
+            this.lbl50.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl50.Location = new System.Drawing.Point(93, 108);
+            this.lbl50.Name = "lbl50";
+            this.lbl50.Size = new System.Drawing.Size(19, 20);
+            this.lbl50.TabIndex = 6;
+            this.lbl50.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 155);
+            this.Controls.Add(this.lbl50);
+            this.Controls.Add(this.lbl20);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSacar);
+            this.Controls.Add(this.txtValor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Button btnSacar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl20;
+        private System.Windows.Forms.Label lbl50;
+    }
+}
+
---
+++ DindinCounter/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DindinCounter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSacar_Click(object sender, EventArgs e)
+        {
+            // Obtém o valor da campo txtValor
+            int valor = int.Parse(txtValor.Text);
+
+            // Se o valor for inválido, exibir mensagem
+            if (valor <= 0)
+            {
+                MessageBox.Show("Valor inválido", "Seu trouxa", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            // Calcula a quantidade de notas de 50;
+            int notas50 = valor / 50;
+            int notas20 = 0;
+
+            // Calcula o resto da divisão por 50
+            int resto50 = valor % 50;
+
+            // Enquanto o resto de 50 for maior que zero
+            while (resto50 > 0)
+            {
+                // Calcula o resto da divisão do resto de 50 por 20
+                int resto20 = resto50 % 20;
+
+                // Se o resto de 20 for maior que zero
+                if (resto20 > 0)
+                {
+                    // Verifica se é possível retirar notas de 50 e adicionar
+                    // ao resto de 50
+                    if ((valor - resto20) >= 50 && notas50 > 0)
+                    {
+                        notas50--;
+                        resto50 += 50;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Valor inválido", "Seu trouxa", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        return;
+                    }
+                }
+                else
+                {
+                    // Calcula a quatidade de notas de 20
+                    notas20 = resto50 / 20;
+                    break;
+                }
+            }
+
+            lbl50.Text = notas50.ToString();
+            lbl20.Text = notas20.ToString();
+
+            #region Versão antiga 
+            /*
+            // Calcular o resto da divisão por 100
+            // Por que 100? Porque 100 é MMC de 50 e 20.
+            float resto100 = valor % 100.0f;
+            float resto20 = 0.0f;
+            float resultValorMenos100 = valor - resto100;
+
+            if (valor < 100)
+            {
+                // Se o resto de 50 for maior que 0, verifica o resto de 20
+                float resto50 = valor % 50;
+                if (resto50 > 0)
+                {
+                    // Se o resto de 50 for menor que 20, significa que o saque
+                    // deve conter apenas notas de 20.
+                    if(resto50 < 20)
+                    {
+                        resto20 = valor % 20;
+                    }
+                    else
+                    {
+                        resto20 = resto50 % 20;
+                    }
+
+                    // Se o resto de 20 for maior que 0, o saque é INVÁLIDO
+                    if (resto20 > 0)
+                    {
+                        MessageBox.Show("O valor é inválido para saque.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                    else
+                    {
+                        float resultValorMenos50 = valor - resto50;
+                        float total50 = 0.0f;
+                        float total20 = 0.0f;
+
+                        if (resto50 >= 20)
+                        {
+                            total50 = resultValorMenos50 / 50.0f;
+                            total20 = resto50 / 20.0f;
+                        }
+                        else
+                        {
+                            total20 = resto100 / 20.0f;
+                        }
+
+                        lbl20.Text = total20.ToString();
+                        lbl50.Text = total50.ToString();
+                    }
+                }
+            }
+            else
+            {
+                // Se o resto de 100 for maior que 0, verifica o resto de 20
+                if (resto100 > 0)
+                {
+                    resto20 = resto100 % 20;
+
+                    // Se o resto de 20 for maior que 0, o saque é INVÁLIDO
+                    if (resto20 > 0)
+                    {
+                        MessageBox.Show("O valor é inválido para saque.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                    else // Se o resto de 20 for 0, o saque é VÁLIDO. Proceder para cálculo.
+                    {
+                        float total50 = resultValorMenos100 / 50.0f;
+                        float total20 = resto100 / 20.0f;
+
+                        lbl20.Text = total20.ToString();
+                        lbl50.Text = total50.ToString();
+                    }
+                }
+                // Se o resto de 50 for 0, está OK e não tem notas de 20
+                else
+                {
+                    float total50 = resultValorMenos100 / 50.0f;
+
+                    lbl20.Text = "0";
+                    lbl50.Text = total50.ToString();
+                }
+            }
+            */
+            #endregion
+        }
+    }
+}
